87948c94dd387d714430987fac2481c903df5edd
meiyaoli <84600758+meiyaoli@users.noreply.github.com>
2025-07-23 20:16:51 -0500

    new changes

--- MD/cincinnati_childrens/script/excel_convert.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("MD/cincinnati_childrens/raw/cincinnati_childrens_hospital.csv", dtype=str)  # Read everything as string
-
-# Save as Excel
-df.to_excel("MD/cincinnati_childrens/raw/cincinnati_childrens_hospital.xlsx", index=False)
